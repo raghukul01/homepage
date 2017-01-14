0f266e8c96a90d0707a7ab6f60bc6fc9b81bf997
raghukul01 <raghukul@iitk.ac.in>
2017-01-15 02:55:21 +0530

    Add files via upload

--- myjavascript.js
+++ myjavascript.js
@@ -1,1 +1,9 @@
-function 
+function click_button(){
+var x=document.getElementById('leftbut')
+if(x.value==='<')x.value='>';
+else x.value='<';
+if(document.getElementById('division2').style.display==="none")
+document.getElementById('division2').style.display="block";
+else 
+document.getElementById('division2').style.display="none";
+}
