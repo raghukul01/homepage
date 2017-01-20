7e57eab86fa146b8374e24f1ce032ada8df349e8
raghukul01 <raghukul@iitk.ac.in>
2017-01-21 01:31:38 +0530

    Add files via upload

--- myjavascript.js
+++ myjavascript.js
@@ -1,9 +1,9 @@
 function click_button(){
-var x=document.getElementById('leftbut')
-if(x.value==='<')x.value='>';
-else x.value='<';
 if(document.getElementById('division2').style.display==="none")
 document.getElementById('division2').style.display="block";
-else 
+else
 document.getElementById('division2').style.display="none";
+}
+function open_resume() {
+  window.open('resume.pdf')
 }
